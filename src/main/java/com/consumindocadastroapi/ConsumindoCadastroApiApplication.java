d71cef85712d72788b2f8bc444ed7925945419d7
claubermartins <clauber789@gmail.com>
2021-08-12 17:52:04 -0300

    iniciando o projeto

---
+++ src/main/java/com/consumindocadastroapi/ConsumindoCadastroApiApplication.java
@@ -0,0 +1,13 @@
+package com.consumindocadastroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumindoCadastroApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumindoCadastroApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/consumindocadastroapi/ConsumindoCadastroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumindocadastroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumindoCadastroApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
